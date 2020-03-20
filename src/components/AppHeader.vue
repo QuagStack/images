9672bd8387959cb1f4626e3299fa562eee892c28
QuagStack <Quagstack@outlook.com>
2020-03-20 08:08:21 -0400

    Initial BP with Header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="/" class="active item"> 
+            Image Storage
+        </a>      
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+
+
+</style>>
+
